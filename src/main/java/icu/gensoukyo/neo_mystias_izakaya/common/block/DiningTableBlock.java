71c0d6210410df5e15ab7684f2e603f504b8a868
Crystal1921 <90130465+Crystal1921@users.noreply.github.com>
2026-07-26 15:49:59 +0800

    添加保温箱和餐桌的模型

--- src/main/java/icu/gensoukyo/neo_mystias_izakaya/common/block/DiningTableBlock.java
+++ src/main/java/icu/gensoukyo/neo_mystias_izakaya/common/block/DiningTableBlock.java
@@ -45,16 +45,7 @@ public class DiningTableBlock extends BaseEntityBlock {
     public static final MapCodec<DiningTableBlock> CODEC = simpleCodec(DiningTableBlock::new);
 
     /** 桌面 (高 14-16) + 四条桌腿 */
-    public static final VoxelShape SHAPE = Shapes.join(
-            Shapes.or(
-                    box(0, 0, 0, 3, 14, 3),    // 西北腿
-                    box(13, 0, 0, 16, 14, 3),   // 东北腿
-                    box(0, 0, 13, 3, 14, 16),   // 西南腿
-                    box(13, 0, 13, 16, 14, 16)  // 东南腿
-            ),
-            box(0, 14, 0, 16, 16, 16),           // 桌面
-            BooleanOp.OR
-    );
+    public static final VoxelShape SHAPE = makeShape();
 
     public DiningTableBlock(Properties properties) {
         super(properties
@@ -171,5 +162,15 @@ public class DiningTableBlock extends BaseEntityBlock {
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
         return SHAPE;
+    }
+
+    public static VoxelShape makeShape(){
+        VoxelShape shape = Shapes.empty();
+        shape = Shapes.join(shape, Shapes.box(0.375, 0, 0.375, 0.625, 0.25, 0.625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.4375, 0.25, 0.4375, 0.5625, 0.8125, 0.5625), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0.3125, 0.8125, 0.3125, 0.6875, 0.875, 0.6875), BooleanOp.OR);
+        shape = Shapes.join(shape, Shapes.box(0, 0.875, 0, 1, 1, 1), BooleanOp.OR);
+
+        return shape;
     }
 }
